af084410b156ee3c154df6d8b9818456b82f9d7f
CHICKIINY <bb.bright36389@gmail.com>
2022-12-27 16:57:03 +0700

    done

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,10 +2,14 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+	double loan,rate,amount;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> amount;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +24,29 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	int year = 1;
+	double pb,i,t,nb;
+	while(1)
+	{
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	cout << setw(13) << left << year;
+	pb = loan; 
+	cout << setw(13) << left << pb;
+	i = pb*(rate/100);
+	cout << setw(13) << left << i;
+	t = pb + i;
+	cout << setw(13) << left << t;
+	if(t < amount){
+		amount = t;
+	}
+	cout << setw(13) << left << amount;
+	nb = t - amount;
+	cout << setw(13) << left << nb;
+	cout << "\n";
+	year++;
+	loan = nb;
+	if(nb == 0)break;
+	}	
 	
 	return 0;
 }
